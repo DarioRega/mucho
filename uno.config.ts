3241134b3e2e1de318492a1943463405de7460bb
DarioRega <dregazzoni@protonmail.com>
2024-11-07 22:22:27 +0100

    init

---
+++ uno.config.ts
@@ -0,0 +1,13 @@
+import { defineConfig } from 'unocss'
+
+export default defineConfig({
+    theme:{
+        fontFamily: {
+            body: 'Helvetica Neue, Helvetica, Arial, sans-serif',
+        },
+    },
+    shortcuts:{
+        'bg-primary':'bg-[#fffff6]',
+        'text-primary':'text-[#d61f26]'
+    }
+})
